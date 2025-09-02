e6395f03ad7b2a879e410f8e42d9cacf10b4c482
spencer-barrett <barrettspencer@outlook.com>
2025-09-02 13:38:40 -0700

    Updates for mobile responsiveness

--- app/frontend/guppylabs/src/components/footer.tsx
+++ app/frontend/guppylabs/src/components/footer.tsx
@@ -1,4 +1,4 @@
-import { Link } from "@heroui/react";
+import { Accordion, AccordionItem, Link } from "@heroui/react";
 import GuppyIcon from "../assets/guppy.svg?react";
 import { Link as RouterLink } from "react-router";
 
@@ -6,12 +6,11 @@ export default function Footer() {
   const currentYear = new Date().getFullYear();
   return (
     <>
-      <footer className="w-svw h-48 border-t-1 border-foreground-200">
-        <div className="flex h-full w-full">
-          <section className=" mx-6 grid grid-cols-3 w-full  divide-x divide-default
-    [&>*:nth-child(3)]:border-l-0">
-            <div className="flex my-6 pr-4 flex-col items-center justify-center">
-              <div className="flex justify-center flex-col">
+      <footer className="max-w-svw w-full border-t-1 border-foreground-200 px-6 sm:px-2 ">
+        <div className="flex w-full">
+          <section className="grid sm:grid-cols-4 grid-rows-3 sm:grid-rows-2 w-full">
+            <div className="flex my-6 flex-col items-center justify-center sm:col-span-2 sm:row-span-2">
+              <div className="flex justify-center flex-col sm:pr-hidden">
                 <span className="inline-flex items-center justify-center gap-2 tracking-widest mb-2">
                   <GuppyIcon height={20} width={20} /> GuppyLabs
                 </span>
@@ -20,7 +19,7 @@ export default function Footer() {
                 </span>
               </div>
             </div>
-            <div className="flex my-6 justify-center">
+            <div className="sm:flex sm:my-6 hidden">
               <div className="flex flex-col">
                 <span className="font-bold">About Us</span>
                 <Link
@@ -33,7 +32,7 @@ export default function Footer() {
                 </Link>
               </div>
             </div>
-            <div className="flex justify-center my-6">
+            <div className="sm:flex sm:my-6 hidden">
               <div className="flex flex-col">
                 <span className="font-bold">Resources</span>
                 <Link
@@ -54,6 +53,40 @@ export default function Footer() {
                 </Link>
               </div>
             </div>
+            <Accordion selectionMode="multiple" className="sm:hidden">
+              <AccordionItem key="1" aria-label="About Us" title="About Us">
+                <div className="">
+                <Link
+                  as={RouterLink}
+                  to={"#"}
+                  className="text-xs mt-2"
+                  color="foreground"
+                >
+                  Contact Us
+                </Link>
+                </div>
+              </AccordionItem>
+              <AccordionItem key="2" aria-label="Resources" title="Resources">
+                <div className="flex flex-col">
+                  <Link
+                    as={RouterLink}
+                    to={"#"}
+                    className="text-xs mt-2"
+                    color="foreground"
+                  >
+                    Our Logs
+                  </Link>
+                  <Link
+                    as={RouterLink}
+                    to={"https://github.com/tonemazz/CS-4800-Web-Project"}
+                    className="text-xs mt-2"
+                    color="foreground"
+                  >
+                    Our Documentation
+                  </Link>
+                </div>
+              </AccordionItem>
+            </Accordion>
           </section>
         </div>
       </footer>

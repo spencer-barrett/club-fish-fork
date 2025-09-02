2e8b58efbb8fcf99c97bc9a0532d45f6d2999815
spencer-barrett <barrettspencer@outlook.com>
2025-09-02 13:41:19 -0700

    Updates for mobile responsiveness

--- app/frontend/guppylabs/src/app/pages/staff.tsx
+++ app/frontend/guppylabs/src/app/pages/staff.tsx
@@ -1,49 +1,40 @@
 import { Avatar, Card, CardBody, CardHeader } from "@heroui/react";
 
 export default function Staff() {
+  const staff = [
+    { role: "Developer/TBD", name: "Spencer" },
+    { role: "Developer/TBD", name: "Tony" },
+    { role: "Developer/TBD", name: "Joshua" },
+    { role: "Developer/TBD", name: "Ethan" },
+  ];
+
   return (
     <main className="w-full flex border-t-1 border-foreground-200 mb-6">
       <div className="flex flex-col items-center w-full mt-6">
-        <span>
+        <span className="text-xl">
           Meet our <span className="font-bold">Team</span>
         </span>
-        <div className="grid grid-rows-2 w-full h-full mt-12">
+        <div className="sm:grid sm:grid-rows-2 flex flex-col w-full h-full mt-12 ">
           <div className="flex justify-center">
-            <Card className="w-[25svw] flex justify-center">
+            <Card className="sm:w-[20svw] w-[55svw] h-[30vh] sm:h-auto flex justify-center">
               <CardHeader className="flex justify-center">
                 <Avatar className="h-24 w-24" name="Michael" />
               </CardHeader>
-              <CardBody></CardBody>
+              <CardBody className="text-center">CEO</CardBody>
             </Card>
           </div>
-          <div className="flex justify-center items-start mt-6">
-            <div className="grid grid-cols-3 gap-1 w-full h-full">
-              <div className="flex justify-center">
-                <Card className="w-[25svw] flex justify-center">
-                  <CardHeader className="flex justify-center">
-                    <Avatar className="h-24 w-24" name="Spencer" />
-                  </CardHeader>
-                  <CardBody></CardBody>
-                </Card>
-              </div>
-              <div className="flex justify-center">
-              <Card className="w-[25svw] flex justify-center">
-                  <CardHeader className="flex justify-center">
-                  <Avatar className="h-24 w-24" name="Tony" />
-                  </CardHeader>
-                  <CardBody></CardBody>
-                </Card>
-                
-              </div>
-              <div className="flex justify-center">
-              <Card className="w-[25svw] flex justify-center">
-                  <CardHeader className="flex justify-center">
-                  <Avatar className="h-24 w-24" name="Josh" />
-                  </CardHeader>
-                  <CardBody></CardBody>
-                </Card>
-                
-              </div>
+          <div className="flex justify-center items-start sm:mt-6 mt-2">
+            <div className="sm:grid sm:grid-cols-4  flex flex-col sm:gap-1 gap-2 w-full h-full">
+              {staff.map((s) => (
+                <div className="flex justify-center">
+                  <Card className="sm:w-[20svw] w-[55svw] h-[30vh] sm:h-auto flex justify-center">
+                    <CardHeader className="flex justify-center">
+                      <Avatar className="h-24 w-24" name={s.name} />
+                    </CardHeader>
+                    <CardBody className="text-center">{s.role}</CardBody>
+                  </Card>
+                </div>
+              ))}
             </div>
           </div>
         </div>

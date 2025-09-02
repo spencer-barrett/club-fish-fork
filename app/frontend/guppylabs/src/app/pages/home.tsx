52df712d3889e22c5e9b407779042bfb3a0760b6
spencer-barrett <barrettspencer@outlook.com>
2025-09-02 15:39:49 -0700

    Hero Banner Update

--- app/frontend/guppylabs/src/app/pages/home.tsx
+++ app/frontend/guppylabs/src/app/pages/home.tsx
@@ -1,14 +1,23 @@
+import { Button } from "@heroui/react";
 import hero from "../../assets/hero.jpg";
 export default function Home() {
   return (
     <div className="flex  w-full flex-col">
       {/* <h1>Home</h1>
       <p>Welcome to Guppylabs</p> */}
-      <section className=" relative h-[50svh] sm:h-svh">
+      <section className=" relative h-svh sm:h-svh">
         <div className="absolute inset-0 bg-black/20 backdrop-blur-[2px] z-200" />
-        <img src={hero} className="absolute inset-0 h-full w-full object-cover" />
+        <img
+          src={hero}
+          className="absolute inset-0 h-full w-full object-cover"
+        />
+        <div className="absolute inset-0 h-full w-full z-[300] flex flex-col justify-center items-center">
+          <p className="px-4 text-3xl sm:text-[4rem] text-center font-[800]">Born in the lab, <br className="sm:hidden"/>raised in the current.</p>
+          <p className="pb-4 pt-2 px-4 text-xs">Building tomorrow’s connections today.</p>
+          <Button className="p-4 text-foreground font-bold" radius="full" color="primary">Learn More</Button>
+        </div>
       </section>
-      <section className="flex sm:h-svh h-[50svh] bg-red-400"></section>
+      <section className="flex sm:h-svh h-svh bg-red-400"></section>
     </div>
   );
 }

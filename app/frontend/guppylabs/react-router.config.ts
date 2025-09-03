7ef1f0dec4e7eb12c931b3136a04a064903293fa
spencer-barrett <barrettspencer@outlook.com>
2025-09-02 20:31:28 -0700

    vercel hosting

--- app/frontend/guppylabs/react-router.config.ts
+++ app/frontend/guppylabs/react-router.config.ts
@@ -1,6 +1,8 @@
 import type { Config } from "@react-router/dev/config";
+import { vercelPreset } from "@vercel/react-router/vite";
 
 export default {
   appDirectory: "./src/app",
   ssr: false,
+  presets: [vercelPreset()],
 } satisfies Config;
